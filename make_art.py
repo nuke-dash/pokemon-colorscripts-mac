3a0492c953cc0ec137c9fa9359dc70c760e88735
Adithyan-KV <adithyankv007@gmail.com>
2021-08-09 02:13:06 +0530

    Added color
     -Now the color image is being printed out to terminal

--- make_art.py
+++ make_art.py
@@ -5,21 +5,28 @@ import matplotlib.pyplot as plt
 
 def main():
     # image = io.imread('bulbasaur.png',as_gray=True)
-    image = io.imread('bulbasaur.png',as_gray=True)
-    image = tm.rescale(image,0.5,anti_aliasing=False)
+    image = io.imread('bulbasaur.png',as_gray=False)
+    # image = tm.rescale(image,0.5,anti_aliasing=False,channel_axis=2)
+    rows,columns,channels = image.shape
+    image = tm.resize(image,(int(rows/2),int(columns/2),channels),anti_aliasing=False,preserve_range=True)
     image = np.repeat(image,2,1)
-    string_matrix = np.full(image.shape,fill_value=' ',dtype=str)
-    rows,columns = string_matrix.shape
-    # string_matrix[image!=1]='@'
-    string_matrix[image!=1]='█'
-    # string_matrix[image==0]='█'
+    print(image.shape)
+    image = image.astype(np.uint8)
+    rows,columns,channels = image.shape
+    string_matrix = np.full((rows,columns),fill_value=' ',dtype=str)
+    print(image[:,:,3].max())
+    string_matrix[image[:,:,3]==255]='█'
+    # string_matrix[image!=1]='█'
+    # # string_matrix[image==0]='█'
     for i in range(rows):
         print('')
         for j in range(columns):
-            color=int(image[i,j]*255)
-            color_escape = get_color_escape(color,color,color,background=False)
+            # print(string_matrix[i,j],end='')
+            r,g,b=image[i,j,:3]
+            color_escape = get_color_escape(r,g,b,background=False)
             print(f'{color_escape}{string_matrix[i,j]}',end='')
     # plt.imshow(image,cmap='gray')
+    # plt.imshow(image)
     # plt.show()
 
 def get_color_escape(r, g, b, background=False):
