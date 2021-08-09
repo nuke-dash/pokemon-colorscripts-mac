92543c931f758fceae4443c9e03ec08a2abfceb0
Adithyan-KV <adithyankv007@gmail.com>
2021-08-09 09:02:50 +0530

    Added command line arguments for help and printing random pokemon
     -Added help text
     -Added interface for printing out random pokemon

--- pokedex.sh
+++ pokedex.sh
@@ -2,14 +2,53 @@
 
 # directory where all the art files exist
 POKEART_DIR='./pokeart'
-# total number of art files present
-NUM_ART=$(ls -1 $POKEART_DIR|wc -l)
+POKELIST_DIR='./scrape'
+# formatting for the help strings
+fmt_help="  %-20s\t%-54s\n"
 
-#selecting a random art file from the whole set
-random_index=$(($RANDOM%$NUM_ART))
-random_pokemon=$(sed $random_index'q;d' './scrape/nameslist.txt')
-pokemon_name=$random_pokemon
 
-# printing out a pokemon
-echo $pokemon_name
-cat $POKEART_DIR'/'$pokemon_name'.txt'
+function _help(){
+    #Function that prints out the help text
+
+    echo "Description: CLI utility to print out unicode image of a pokemon in your shell"
+    echo ""
+    echo "Usage: pokedex [OPTION] [POKEMON NAME]"
+    printf "${fmt_help}" \
+        "-h, --help, help" "Print this help." \
+        "-r, --random, random" "Show a random pokemon"
+}
+
+function _show_random_pokemon(){
+    #selecting a random art file from the whole set
+
+    # total number of art files present
+    NUM_ART=$(ls -1 $POKEART_DIR|wc -l)
+    # getting a random index from 0-NUM_ART
+    random_index=$(($RANDOM%$NUM_ART))
+    random_pokemon=$(sed $random_index'q;d' $POKELIST_DIR'/nameslist.txt')
+    echo $random_pokemon
+
+    # print out the pokemon art for the pokemon
+    cat $POKEART_DIR'/'$random_pokemon'.txt'
+}
+# Handling command line arguments
+case "$#" in
+    0)
+        # display help if no arguments are given
+        _help
+        ;;
+    1)
+        case "$1" in
+            -h | --help | help)
+                _help
+                ;;
+            -r | --random | random)
+                _show_random_pokemon
+                ;;
+            *)
+                echo "Input error."
+                exit 1
+                ;;
+        esac
+        ;;
+esac
